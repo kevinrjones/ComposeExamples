bbe6b12da31c2f2b00888648917c7c78784d9fc4
Kevin <kevin@rocksolidknowledge.com>
2021-06-06 10:02:48 +0100

    Updated to fix some build issues

---
+++ sudokusolver/build.gradle.kts
@@ -0,0 +1,37 @@
+import org.jetbrains.compose.compose
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.10"
+    id("org.jetbrains.compose") version "0.4.0"
+}
+
+group = "com.knowledgespike"
+version = "1.0"
+
+repositories {
+    jcenter()
+    mavenCentral()
+    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
+}
+
+dependencies {
+    implementation(compose.desktop.currentOs)
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "11"
+}
+
+compose.desktop {
+    application {
+        mainClass = "com.knowledgespike.sudokusolver.MainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "sudokusolver"
+            packageVersion = "1.0.0"
+        }
+    }
+}
